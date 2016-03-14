7556416281bc5de72109f9559dafdba30d44cf40
398058068@qq.com <398058068@qq.com>
2016-03-14 15:17:05 +0800

    1.更新spring aop依赖 2.添加druid监控

--- src/main/java/com/yhbj/controller/DemoController.java
+++ src/main/java/com/yhbj/controller/DemoController.java
@@ -19,14 +19,15 @@ import java.util.List;
 public class DemoController {
     @Autowired
     DemoService demoService;
+
     @RequestMapping("/")
     public String index(Model model,
                         @RequestParam(required = false, defaultValue = "1") int page,
                         @RequestParam(required = false, defaultValue = "10") int rows) {
         PageHelper.startPage(page, rows);
         List<Demo> demoList = demoService.selectAll();
-        model.addAttribute("demoList",demoList);
-        model.addAttribute("page",new PageInfo(demoList));
+        model.addAttribute("demoList", demoList);
+        model.addAttribute("page", new PageInfo(demoList));
         return "index";
     }
 }
